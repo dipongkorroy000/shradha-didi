58abb52b41f672a5ecfb4d09de7b632da38c8ec5
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-10 00:19:02 +0600

    sharadha didi

---
+++ first2.js
@@ -0,0 +1,136 @@
+// let age = 16;
+// if(age > 18) {
+//     console.log("you can vote");
+// } 
+// if( age < 18) {
+//     console.log("you can not vote");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// }
+// if (mode === "light"){
+//     color = "white"
+// }
+// console.log(color);
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark"){
+//     color = "black";
+// } else{
+//     color = "white"
+// }
+// console.log(color);
+
+// let age = 16;
+// if (age > 18){
+//     console.log("you can vote");
+// } else{
+//     console.log("you can't vote");
+// }
+
+// even of odd
+// let num = 10;
+// if(num % 2 === 0){
+//     console.log(num, "is even");
+// } else{
+//     console.log(num, "is odd");
+// }
+
+// let mode = "silver";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// } 
+// else if( mode === "blue"){
+//     color = "blue";
+// }
+// else if (mode == "pink"){
+//     color = "pink";
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color)
+
+// conditional Statement 
+// let age = 16;
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);
+
+// alert("Hello!") //one time popup
+// prompt("Hello!") //input want
+
+// let fullName = prompt("hello!")
+// console.log(fullName)
+
+// let num = prompt("enter a number");
+// if(num % 5 === 0){
+//     console.log(num, "is a multiple of 5");
+// }
+// else{
+//     console.log(num, "is not a multiple of 5")
+// }
+
+// practice 
+// let score = 70;
+// let grade;
+// if(score >= 90 && score <= 100){
+//     grade = "A++";
+// }
+// else if(score >= 80 && score <= 89){
+//     grade = "A+";
+// }
+// else if(score >= 70 && score <= 79){
+//     grade = "A";
+// }
+// else if(score >= 60 && score <= 69){
+//     grade = "B";
+// }
+// else if(score >= 50 && score <= 59){
+//     grade = "C";
+// }
+// else if(score >= 40 && score <= 49){
+//     grade = "D";
+// }
+// else if(score >= 33 && score <= 39){
+//     grade = "E";
+// }
+// else if(score <= 32){
+//     grade = "F";
+// }
+// console.log("according to your score, your grade was :", grade)
+
+
+// let score = prompt("enter your score(0-100):");
+// let grade;
+// if(score >= 90 && score <= 100){
+//     grade = "A++";
+// }
+// else if(score >= 80 && score <= 89){
+//     grade = "A+";
+// }
+// else if(score >= 70 && score <= 79){
+//     grade = "A";
+// }
+// else if(score >= 60 && score <= 69){
+//     grade = "B";
+// }
+// else if(score >= 50 && score <= 59){
+//     grade = "C";
+// }
+// else if(score >= 40 && score <= 49){
+//     grade = "D";
+// }
+// else if(score >= 33 && score <= 39){
+//     grade = "E";
+// }
+// else if(score <= 32){
+//     grade = "F";
+// }
+// console.log("according to your score, your grade was :", grade)
