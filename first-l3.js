62b072268b681023f3513790194a5505f7ac1caa
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-18 13:04:44 +0600

    add this file

---
+++ first-l3.js
@@ -0,0 +1,47 @@
+/**
+ * loop--->
+ * for loop---
+ * while loop---
+ * do while loop---
+ */
+
+// for(let i = 0;i <=10;i++){
+//     console.log(i);
+// }
+
+//alternative--
+// let i = 0;
+// while(i <=10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i < 10);
+
+// let strVar = 'dipongkor'; 
+// for(let value of strVar){
+//     console.log('alphabet:',value);
+// }
+
+// let fullName = 'finalExam';
+// let fullNameLength = 0;
+// for(let i of fullName){
+//     console.log('i=',i);
+//     fullNameLength++
+// }
+// console.log(fullNameLength);
+
+// let student = {
+//     fullName: 'Rahul Kumar',
+//     age: 20,
+//     cgpa: 7.5,
+//     isPass: true
+// }
+// for(let key in student){
+//     console.log(key,student[key]);
+// }
