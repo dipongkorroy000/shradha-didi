9207f2376b4b209f415a5d062fcd4421d6630cc3
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-19 00:04:11 +0600

    add file

---
+++ first-l4.js
@@ -0,0 +1,29 @@
+// let marks = [97, 25, 49, 34];
+// let letter = ['java', 'javascript', 'python', 'c++'];
+// console.log(marks.toString());
+
+//for loop--
+// for (let i = 0; i < letter.length; i++){
+//     console.log(letter[i]);
+// }
+
+//for of--
+// for (let item of letter) {
+//     console.log(item);
+// }
+
+//problems--
+// let arrayNum = [250, 645, 300, 900, 50];
+// let newArray = [];
+// for (let price of arrayNum) {
+//     newArray.push(price - (price * 0.1));
+// }
+// console.log(newArray);
+
+//alternative--
+// let arrayNum = [250, 645, 300, 900, 50];
+// for (let i = 0; i < arrayNum.length; i++){
+//     let offer = arrayNum[i] / 10;
+//     arrayNum[i] -= offer;
+// }
+// console.log(arrayNum);
